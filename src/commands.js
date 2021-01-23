a2994a60e16e4d4eecfcbc97ba38a6e921002fb4
Esteban GT <egonzalezt@eafit.edu.co>
2021-01-23 16:58:07 -0500

    Improved hello function

--- src/commands.js
+++ src/commands.js
@@ -89,7 +89,7 @@ function message(message)
     .then(msg => 
         {
             msg.react(LapisEmoji.Lapis1.Emoji)
-            msg.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.id == LapisEmoji.Lapis15.id),
+            message.awaitReactions((reaction, user) => user.id == message.author.id && (reaction.emoji.id == LapisEmoji.Lapis15.id),
                 { max: 1, time: 10000 }).then(collected => {
                     if (collected.first().emoji.id == LapisEmoji.Lapis15.id) {
                         message.channel.send("https://tenor.com/view/steven-universe-lapis-lazuli-gif-7334165");
