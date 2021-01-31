9a8ee4b39a3c06b984ec311b0f9ad625d9e39d5c
Esteban GT <egonzalezt@eafit.edu.co>
2021-01-31 14:02:13 -0500

    Fix ?urequest function

--- src/commands.js
+++ src/commands.js
@@ -454,7 +454,7 @@ function userequest(message,args)
 {
     try
     {
-        if(message.mentions.users.first())
+        if(message.mentions.users.first()) 
         {
             let user1 = message.mentions.users.first();
             if(!user1.bot)
@@ -464,13 +464,13 @@ function userequest(message,args)
                 user1.send(message.author.username+" needs you on "+message.channel.name+" From "+message.channel.guild.name+" Server and says \n"+messageU)
                 .then(msg => {
                     msg.react(LapisEmoji.Lapis15.Emoji)
-                        msg.awaitReactions((reaction, user) => (reaction.emoji.id == LapisEmoji.Lapis15.id),
-                                { max: 2, time: 10000 }).then(collected => {
+                        msg.awaitReactions((reaction, user) => user.id == user1.id &&  (reaction.emoji.id == LapisEmoji.Lapis15.id),
+                                { max: 1, time: 10000 }).then(collected => {
                                     if (collected.first().emoji.id == LapisEmoji.Lapis15.id) {
                                         message.author.send("Hey "+message.author.username+", "+msg.channel.recipient.username+" React to your message.");
                                     }
                             }).catch(() => {});
-                });
+                }).then((msg1)=>{message.author.send("Hey "+message.author.username+", Your request have send have a good day :)");});
             }
             else
             {
