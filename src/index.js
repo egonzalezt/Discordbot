aba7d8de5608eda20a8baca250a101978e299ab1
Esteban GT <egonzalezt@eafit.edu.co>
2021-02-09 21:37:38 -0500

    fix index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ bot.on('ready', () => {
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-var p = "app/src/commands/"
+var p = "/app/src/commands/"
 fs.readdir(p, function (err, files) {
     if (err) {
         throw err;
@@ -33,7 +33,7 @@ fs.readdir(p, function (err, files) {
         return fs.statSync(file).isDirectory();
     }).forEach(function (file) {
         var ruta = file.replace("\\","/")
-        let directory =`./${ruta}/`
+        let directory =`.${ruta}/`
         console.log(directory);
         fs.readdir(directory, (err, files) => {
 
